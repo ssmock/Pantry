cccb62b58c5564ff917d14960cba8d882a75086f
ssmock <ssmock@gmail.com>
2014-11-09 17:58:05 -0500

    Added base test with connections/transactions setup/teardown; added test for GetUnits

---
+++ PantryTests/Common/TestGetUnits.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pantry.Common;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace PantryTests.Common
+{
+    [TestClass]
+    public class TestGetUnits: PantryTestBase
+    {
+        [TestMethod]
+        public void GetUnits_Gets_All_Units()
+        {
+            var units =
+                (new GetUnits()).Query(connection, transaction);
+
+            Assert.IsTrue(units.Count() > 0);
+        }
+    }
+}
---
+++ PantryTests/PantryTestBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace PantryTests
+{
+    [TestClass]
+    public class PantryTestBase
+    {
+        protected IDbConnection connection;
+        protected IDbTransaction transaction;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            connection = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["PantryTest"]
+                    .ConnectionString);
+
+            connection.Open();
+
+            transaction = connection.BeginTransaction();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            transaction.Rollback();
+            connection.Close();
+        }
+    }
+}
