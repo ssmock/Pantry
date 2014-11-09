b0a57dd40f6b5d827e7b89d079ff2b48e5c3f4f1
ssmock <ssmock@gmail.com>
2014-11-09 17:12:31 -0500

    Added schemas; created the rollback script

---
+++ Pantry/DatabaseSetup/back.1.everything.sql
@@ -0,0 +1,52 @@
+﻿set nocount on
+GO
+
+if not exists (select 1 from sys.schemas where name = 'Stock')
+begin
+    print 'Schema "Stock" does not exist to drop.'
+end
+else
+begin
+    drop schema Stock
+
+    print 'Schema "Stock" was dropped.'
+end
+GO
+
+if not exists (select 1 from sys.schemas where name = 'Common')
+begin
+    print 'Schema "Common" does not exist to drop.'
+end
+else
+begin
+    drop schema Common
+
+    print 'Schema "Common" was dropped.'
+end
+GO
+
+if not exists (select 1 from sys.schemas where name = 'Access')
+begin
+
+    print 'Schema "Access" does not exist to drop.'
+end
+else
+begin
+    drop schema Access
+
+    print 'Schema "Access" already exists.'
+end
+GO
+
+use master
+
+if exists (select 1 from sys.databases where name = 'Pantry')
+begin
+    drop database Pantry
+
+    print 'Database "Pantry" was dropped.'
+end
+else
+begin
+    print 'Database "Pantry" does not exist to drop.'
+end
---
+++ Pantry/DatabaseSetup/forward.2.schemas.sql
@@ -0,0 +1,43 @@
+﻿set nocount on
+GO
+
+-- "Stock" is our pantry's transactional product domain.
+if not exists (select 1 from sys.schemas where name = 'Stock')
+begin
+	exec sp_executesql N'create schema Stock'
+
+	print 'Schema "Stock" was created.'
+end
+else
+begin
+	print 'Schema "Stock" already exists.'
+end
+GO
+
+-- "Common" is our pantry's shared reference domain.  We'll put
+-- common lookups, like units of measure, here.
+if not exists (select 1 from sys.schemas where name = 'Common')
+begin
+	exec sp_executesql N'create schema Common'
+
+	print 'Schema "Common" was created.'
+end
+else
+begin
+	print 'Schema "Common" already exists.'
+end
+GO
+
+-- "Access" is our application authorization domain.  We will
+-- key to its PantryUser table frequently.
+if not exists (select 1 from sys.schemas where name = 'Access')
+begin
+	exec sp_executesql N'create schema Access'
+
+	print 'Schema "Access" was created.'
+end
+else
+begin
+	print 'Schema "Access" already exists.'
+end
+GO
