fd90997bf11d0a14041a37157885c00f3bc5ccb9
ssmock <ssmock@gmail.com>
2014-11-09 17:59:27 -0500

    Added the query type for GetUnits; brought in Dapper

---
+++ Pantry/Common/GetUnits.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Pantry.Common
+{
+    public class GetUnits
+    {
+        public class Result
+        {
+            public string Name { get; set; }
+            public decimal StandardConversion { get; set; }
+            public decimal MetricConversion { get; set; }
+            public bool IsMetric { get; set; }
+        }
+
+        public IEnumerable<Result> Query(
+            IDbConnection connection, 
+            IDbTransaction transaction)
+        {
+            return
+                connection.Query<Result>(
+                    "Common.GetUnits",
+                    commandType: CommandType.StoredProcedure,
+                    param: this,
+                    transaction: transaction);
+        }
+
+        public bool IncludeCustomary { get; set; }
+    }
+}
